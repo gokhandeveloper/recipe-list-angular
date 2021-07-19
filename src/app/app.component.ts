289d46b05f80f041aa28aaca6d0da666066ca559
gokhandilek <gokhanengineering@gmail.com>
2021-07-19 11:49:19 +0800

    recipes are now clickable in the list.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'courseproject-the-basics';
+
+  loadedFeauture = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeauture = feature;
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Recipe} from "../recipe.model";
 
 @Component({
@@ -8,12 +8,13 @@ import {Recipe} from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[]=[
     new Recipe("A test recipe",
       "description",
       "https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg"),
-    new Recipe("A test recipe",
-      "description",
+    new Recipe("A test recipe 2",
+      "description 2",
       "https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg")
 
   ];
@@ -23,4 +24,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipeElement: Recipe) {
+    this.recipeWasSelected.emit(recipeElement);
+  }
 }
